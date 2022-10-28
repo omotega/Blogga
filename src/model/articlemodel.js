8941858ea0ca7e7c59b8da403af69f4b59eb1637
omotega <tomoyibo@gmail.com>
2022-10-28 20:33:57 +0100

    made update to the articlemodel file

---
+++ src/model/articlemodel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+
+const articleSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  body: { type: String, required: true },
+  author: { type: String, required: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  state: { type: String, default: 'draft', enum: ['draft', 'published'] },
+  readCount: { type: Number, default: 0 },
+  readingTime: { type: String },
+  tags: [String],
+}, {
+  timestamps: true,
+})
+
+
+module.exports = mongoose.model('Article', articleSchema);
