d5386f7aa19bc829c15fce4fdb804293fd65bc20
omotega <tomoyibo@gmail.com>
2022-10-28 01:00:50 +0100

    added user authentication to the application

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const User = require('../model/usermodel');
+const { decodeToken } = require('../utils/token')
+
+
+const authguard = async(req,res,next) => {
+    try {
+        if(req.headers && req.headers.authorization) {
+            const token = await req.headers.authorization.split(' ')[1];
+            const decode = await decodeToken(token);
+            const user = await User.findById(decode.id);
+            if(!user) return errorResponse(res,401,'user not found');
+            req.user = user;
+            console.log(req.user);
+            return next()
+        } else {
+            return errorResponse(res,401,'authorization not found');
+        }
+    } catch (error) {
+        return errorResponse(res,500,error.message);
+    }
+}
+
+module.exports = {
+    authguard,
+}
