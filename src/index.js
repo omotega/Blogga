d1a4cdebb12c232cfb29c216ddba8f7e9b64d21a
omotega <tomoyibo@gmail.com>
2022-10-29 11:08:16 +0100

    added article router to the index.js file

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ const port = process.env.PORT;
 
 const { Dbconnect } = require('./config/db');
 const userRouter = require('./routes/userroute');
+const articleRouter = require('./routes/articleroute');
+const { application } = require('express');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.get('/',(req,res) => {
-//   res.send('welcome to my blog application');
-// })
+app.get('/',(req,res) => {
+  res.send('welcome to my blog application');
+})
 
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/articles',articleRouter);
 
 Dbconnect();
 
