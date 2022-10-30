5b085d4840cd0b1f4ae3d0a78367868635d7bad6
omotega <tomoyibo@gmail.com>
2022-10-30 04:28:11 +0100

    added delete route to my article route

--- src/routes/articleroute.js
+++ src/routes/articleroute.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const articleRouter = express.Router();
 
-const { createArticle,getAllArticles,editArticle ,getAllArticleById,updateArticle } = require('../controllers/articlecontroller');
+const { createArticle,getAllArticles,editArticle ,getAllArticleById,updateArticle,deleteArticle } = require('../controllers/articlecontroller');
 const { authguard } = require('../middleware/auth')
 
 articleRouter.route('/').post(authguard,createArticle).get(getAllArticles);
-articleRouter.route('/:articleId').patch(authguard,editArticle).get(getAllArticleById).put(authguard,updateArticle);
+articleRouter.route('/:articleId').patch(authguard,editArticle).get(getAllArticleById).put(authguard,updateArticle).delete(authguard,deleteArticle);
 
 module.exports = articleRouter;
 
