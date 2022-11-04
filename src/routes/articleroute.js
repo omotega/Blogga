4c1a41f8412fac0ca6656e3c8a71d6f0fac330c2
omotega <tomoyibo@gmail.com>
2022-11-04 12:04:43 +0100

    updatedv the article route

--- src/routes/articleroute.js
+++ src/routes/articleroute.js
@@ -3,11 +3,15 @@ const express = require('express');
 
 const articleRouter = express.Router();
 
-const { createArticle,getAllArticles,editArticle ,getAllArticleById,updateArticle,deleteArticle } = require('../controllers/articlecontroller');
+const { createArticle,getAllArticles,editArticle ,getAllArticleById,updateArticle,deleteArticle,getAllUserArticle } = require('../controllers/articlecontroller');
 const { authguard } = require('../middleware/auth')
 
 articleRouter.route('/').post(authguard,createArticle).get(getAllArticles);
+articleRouter.route('/userblog').get(authguard,getAllUserArticle);
 articleRouter.route('/:articleId').patch(authguard,editArticle).get(getAllArticleById).put(authguard,updateArticle).delete(authguard,deleteArticle);
+
+
+
 
 module.exports = articleRouter;
 
