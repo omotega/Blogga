a2802af82135d4f86446ec4f323d76f5b33a855d
omotega <tomoyibo@gmail.com>
2022-11-05 19:20:40 +0100

    updated my auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,25 +1,24 @@
 const User = require('../model/usermodel');
-const { decodeToken } = require('../utils/token')
-const { errorResponse } = require('../utils/responses')
+const { decodeToken } = require('../utils/token');
+const { errorResponse } = require('../utils/responses');
 
-
-const authguard = async(req,res,next) => {
-    try {
-        if(req.headers && req.headers.authorization) {
-            const token = await req.headers.authorization.split(' ')[1];
-            const decode = await decodeToken(token);
-            const user = await User.findById(decode.id);
-            if(!user) return errorResponse(res,401,'user not found');
-            req.user = user;
-            return next()
-        } else {
-            return errorResponse(res,401,'authorization not found');
-        }
-    } catch (error) {
-        return errorResponse(res,500,error.message);
+const authguard = async (req, res, next) => {
+  try {
+    if (req.headers && req.headers.authorization) {
+      const token = await req.headers.authorization.split(' ')[1];
+      const decode = await decodeToken(token);
+      const user = await User.findById(decode.id);
+      if (!user) return errorResponse(res, 401, 'user not found');
+      req.user = user;
+      return next();
+    } else {
+      return errorResponse(res, 401, 'authorization not found');
     }
-}
+  } catch (error) {
+    return errorResponse(res, 500, error.message);
+  }
+};
 
 module.exports = {
-    authguard,
-}
+  authguard,
+};
