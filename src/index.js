566fc8ca54239dffa3fac262127a81a3c0740b84
omotega <tomoyibo@gmail.com>
2022-11-05 21:05:59 +0100

    updated the index.js file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
+const { Dbconnect } = require('./config/db')
+const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require('express');
+
 const port = process.env.PORT;
-
-const { Dbconnect } = require('./config/db');
-const userRouter = require('./routes/userroute');
-const articleRouter = require('./routes/articleroute');
-
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get('/',(req,res) => {
-  res.send('welcome to my blog application');
-})
-
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/articles',articleRouter);
 
 Dbconnect();
 
