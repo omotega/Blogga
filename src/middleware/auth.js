ac4ffe71bdad4e84e6aa44a210958ee2059add60
omotega <tomoyibo@gmail.com>
2023-01-06 11:51:45 +0100

    refactor:edited the helper function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,12 @@
 const User = require('../model/usermodel');
-const { decodeToken } = require('../utils/token');
+const { Helper } = require('../utils/genutils');
 const { errorResponse } = require('../utils/responses');
 
 const authguard = async (req, res, next) => {
   try {
     if (req.headers && req.headers.authorization) {
       const token = await req.headers.authorization.split(' ')[1];
-      const decode = await decodeToken(token);
+      const decode = await Helper.decodeToken(token);
       const user = await User.findById(decode.id);
       if (!user) return errorResponse(res, 401, 'user not found');
       req.user = user;
